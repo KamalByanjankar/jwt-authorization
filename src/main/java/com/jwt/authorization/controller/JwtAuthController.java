963eed18a9a7cf68fed162b914f97b75e01335da
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-04-06 20:21:48 +0200

    filter added

--- src/main/java/com/jwt/authorization/controller/JwtAuthController.java
+++ src/main/java/com/jwt/authorization/controller/JwtAuthController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -59,6 +60,10 @@ public class JwtAuthController {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword()));
 		}
 		catch(UsernameNotFoundException e){
+			e.printStackTrace();
+			throw new Exception("Bad Credentials");
+		}
+		catch(BadCredentialsException e){
 			e.printStackTrace();
 			throw new Exception("Bad Credentials");
 		}
--- src/main/java/com/jwt/authorization/utility/JwtUtils.java
+++ src/main/java/com/jwt/authorization/utility/JwtUtils.java
@@ -1,16 +1,20 @@
 package com.jwt.authorization.utility;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
+import com.jwt.authorization.service.UserDetailsImpl;
+
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 
 @Component
@@ -18,42 +22,42 @@ public class JwtUtils {
 	
 	public static final long JWT_TOKEN_VALIDITY = 1000* 60 * 60 * 5;
 	
-	 private String SECRET_KEY = "secret";
+	private String SECRET_KEY = "secret";
+	
+	private static final Logger logger  = LoggerFactory.getLogger(JwtUtils.class);
 
-	    public String extractUsername(String token) {
-	        return extractClaim(token, Claims::getSubject);
-	    }
+	public String generateJwtToken(Authentication authentication) {
 
-	    public Date extractExpiration(String token) {
-	        return extractClaim(token, Claims::getExpiration);
-	    }
+		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
-	    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-	        final Claims claims = extractAllClaims(token);
-	        return claimsResolver.apply(claims);
-	    }
-	    private Claims extractAllClaims(String token) {
-	        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
-	    }
+		return Jwts.builder()
+				.setSubject((userPrincipal.getUsername()))
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + JWT_TOKEN_VALIDITY))
+				.signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+				.compact();
+	}
 
-	    private Boolean isTokenExpired(String token) {
-	        return extractExpiration(token).before(new Date());
-	    }
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+	}
 
-	    public String generateToken(UserDetails userDetails) {
-	        Map<String, Object> claims = new HashMap<>();
-	        return createToken(claims, userDetails.getUsername());
-	    }
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException e) {
+			logger.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (MalformedJwtException e) {
+			logger.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			logger.error("JWT token is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			logger.error("JWT token is unsupported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			logger.error("JWT claims string is empty: {}", e.getMessage());
+		}
 
-	    private String createToken(Map<String, Object> claims, String subject) {
-
-	        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-	                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
-	                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-	    }
-
-	    public Boolean validateToken(String token, UserDetails userDetails) {
-	        final String username = extractUsername(token);
-	        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-	    }
+		return false;
+	}
 }
